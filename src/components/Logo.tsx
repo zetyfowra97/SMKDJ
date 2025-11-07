2d2d93beb0b6b88153bff3b37b1a665585ddd48c
zetyfowra97 <135099915+zetyfowra97@users.noreply.github.com>
2025-11-07 08:43:16 +0800

    New 8.43 7/11

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,87 +4,146 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import { Navigation, Autoplay, Virtual } from 'swiper';
-import { Link } from 'react-router-dom';
 
 const Logo = () => {
     return (
-        <div  className="pb-20 pt-20 bg-white">
-        <section>
-            
-            <div className="mx-auto w-4/5 py-10">
-                <Swiper
-                    modules={[Navigation, Autoplay, Virtual]}
-                    slidesPerView="auto"
-                    spaceBetween={30}
-                    speed={2500}
-                    loop={true}
-                    autoplay={{ delay: 0, disableOnInteraction: false }}
-                    breakpoints={{
-                        320: {
-                            slidesPerView: 1.7,
-                        },
-                        600: {
-                            slidesPerView: 3,
-                        },
-                        1000: {
-                            slidesPerView: 4,
-                        },
-                        1600: {
-                            slidesPerView: 6,
-                        },
-                    }}
-                    dir={'ltr'}
-                    key={'false'}
-                >
-                    <SwiperSlide>
-                        <Link to="#" className="flex items-center justify-center gap-3">
-                            <img className="h-full" src="assets/images/SCHOOLEMAIL.png" alt="" />
-                        </Link>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <Link to="#" className="flex items-center justify-center gap-3">
-                            <img className="h-full" src="assets/images/DELIMa.png" alt="" />
-                        </Link>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <Link to="#" className="flex items-center justify-center gap-3">
-                            <img className="h-full" src="assets/images/e-pangkat.png" alt="" />
-                        </Link>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <Link to="#" className="flex items-center justify-center gap-3">
-                            <img className="h-full" src="assets/images/SPLKPM.png" alt="" />
-                        </Link>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <Link to="#" className="flex items-center justify-center gap-3">
-                            <img className="h-full" src="assets/images/APDM.png" alt="" />
-                        </Link>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <Link to="#" className="flex items-center justify-center gap-3">
-                            <img className="h-full" src="assets/images/HRMIS.png" alt="" />
-                        </Link>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <Link to="#" className="flex items-center justify-center gap-3">
-                            <img className="h-full" src="assets/images/e-operasi.png" alt="" />
-                        </Link>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <Link to="#" className="flex items-center justify-center gap-3">
-                            <img className="h-full" src="assets/images/SAPS.png" alt="" />
-                        </Link>
-                    </SwiperSlide>
-                    <SwiperSlide>
-                        <Link to="#" className="flex items-center justify-center gap-3">
-                            <img className="h-full" src="assets/images/ANM.png" alt="" />
-                        </Link>
-                    </SwiperSlide>
-                    
-                </Swiper>
-            </div>
-        </section>
+        <div className="pb-20 pt-20 bg-white">
+            <section>
+                <div className="mx-auto w-4/5 py-10">
+                    <Swiper
+                        modules={[Navigation, Autoplay, Virtual]}
+                        slidesPerView="auto"
+                        spaceBetween={30}
+                        speed={2500}
+                        loop={true}
+                        autoplay={{ delay: 0, disableOnInteraction: false }}
+                        breakpoints={{
+                            320: {
+                                slidesPerView: 1.7,
+                            },
+                            600: {
+                                slidesPerView: 3,
+                            },
+                            1000: {
+                                slidesPerView: 4,
+                            },
+                            1600: {
+                                slidesPerView: 6,
+                            },
+                        }}
+                        dir={'ltr'}
+                        key={'false'}
+                    >
+                        {/* School Email */}
+                        <SwiperSlide>
+                            <a 
+                                href="mailto:bea8658@moe.edu.my" 
+                                target="_blank" 
+                                rel="noopener noreferrer" 
+                                className="flex items-center justify-center gap-3"
+                            >
+                                <img className="h-full" src="assets/images/SCHOOLEMAIL.png" alt="School Email" />
+                            </a>
+                        </SwiperSlide>
+
+                        {/* DELIMa */}
+                        <SwiperSlide>
+                            <a 
+                                href="https://delima.moe-dl.edu.my/" 
+                                target="_blank" 
+                                rel="noopener noreferrer"
+                                className="flex items-center justify-center gap-3"
+                            >
+                                <img className="h-full" src="assets/images/DELIMa.png" alt="DELIMa" />
+                            </a>
+                        </SwiperSlide>
+
+                        {/* e-Pangkat */}
+                        <SwiperSlide>
+                            <a 
+                                href="https://epangkat.moe.gov.my/login.php" 
+                                target="_blank" 
+                                rel="noopener noreferrer"
+                                className="flex items-center justify-center gap-3"
+                            >
+                                <img className="h-full" src="assets/images/e-pangkat.png" alt="e-Pangkat" />
+                            </a>
+                        </SwiperSlide>
+
+                        {/* SPLKPM */}
+                        <SwiperSlide>
+                            <a 
+                                href="https://splkpm.moe.gov.my/index.cfm" 
+                                target="_blank" 
+                                rel="noopener noreferrer"
+                                className="flex items-center justify-center gap-3"
+                            >
+                                <img className="h-full" src="assets/images/SPLKPM.png" alt="SPLKPM" />
+                            </a>
+                        </SwiperSlide>
+
+                        {/* APDM */}
+                        <SwiperSlide>
+                            <a 
+                                href="https://apdm.moe.gov.my/" 
+                                target="_blank" 
+                                rel="noopener noreferrer"
+                                className="flex items-center justify-center gap-3"
+                            >
+                                <img className="h-full" src="assets/images/APDM.png" alt="APDM" />
+                            </a>
+                        </SwiperSlide>
+
+                        {/* HRMIS */}
+                        <SwiperSlide>
+                            <a 
+                                href="https://hrmis2.eghrmis.gov.my/HRMISNET/Common/Main/Login.aspx" 
+                                target="_blank" 
+                                rel="noopener noreferrer"
+                                className="flex items-center justify-center gap-3"
+                            >
+                                <img className="h-full" src="assets/images/HRMIS.png" alt="HRMIS" />
+                            </a>
+                        </SwiperSlide>
+
+                        {/* e-Operasi */}
+                        <SwiperSlide>
+                            <a 
+                                href="https://eoperasi.moe.gov.my/" 
+                                target="_blank" 
+                                rel="noopener noreferrer"
+                                className="flex items-center justify-center gap-3"
+                            >
+                                <img className="h-full" src="assets/images/e-operasi.png" alt="e-Operasi" />
+                            </a>
+                        </SwiperSlide>
+
+                        {/* SAPS */}
+                        <SwiperSlide>
+                            <a 
+                                href="https://sapsnkra.moe.gov.my/" 
+                                target="_blank" 
+                                rel="noopener noreferrer"
+                                className="flex items-center justify-center gap-3"
+                            >
+                                <img className="h-full" src="assets/images/SAPS.png" alt="SAPS" />
+                            </a>
+                        </SwiperSlide>
+
+                        {/* ANM */}
+                        <SwiperSlide>
+                            <a 
+                                href="https://epenyatagaji-laporan.anm.gov.my/Layouts/Login/Login.aspx" 
+                                target="_blank" 
+                                rel="noopener noreferrer"
+                                className="flex items-center justify-center gap-3"
+                            >
+                                <img className="h-full" src="assets/images/ANM.png" alt="ANM" />
+                            </a>
+                        </SwiperSlide>
+                    </Swiper>
+                </div>
+            </section>
         </div>
     );
 };
--- src/pages/Makluman.tsx
+++ src/pages/Makluman.tsx
@@ -1,42 +1,41 @@
-import React, { useRef, useState } from 'react';
-import Lightbox from 'react-18-image-lightbox';
+import React, { useState } from 'react';
 import Zoom from 'react-medium-image-zoom';
 import 'react-medium-image-zoom/dist/styles.css';
 
 const Makluman = () => {
-    const [isOpen, setIsOpen] = useState<any>(false);
-    const [photoIndex, setPhotoIndex] = useState<any>(0);
+  const [isOpen, setIsOpen] = useState(false);
 
-    return (
-        <div className="bg-white">
-        <div className="pt-20 pb-20">
-            <h1 className="text-5xl text-center font-extrabold text-white mb-10 tracking-widest text-[#90d6fa]">SPM 2025</h1>
-            <div>
-                
-                <div className="col-span-4 justify-items-center col-start-2 md:col-span-2 md:col-start-3" onClick={() => setIsOpen(true)}>
-                    <Zoom>
-                        <img src="/assets/images/SuratMaklumanTaklimatPakejTingkatan4.jpeg" alt="" />
-                    </Zoom>
-                </div>
-                 <div className="col-span-4 justify-items-center col-start-2 md:col-span-2 md:col-start-3" onClick={() => setIsOpen(true)}>
-                    <Zoom>
-                        <img src="/assets/images/SURATMAKLUMANPERUBAHANJADUALPINTASTINGKATN42025.jpeg" alt="" />
-                    </Zoom>
-                </div>
-                 <div className="col-span-4 justify-items-center col-start-2 md:col-span-2 md:col-start-3" onClick={() => setIsOpen(true)}>
-                    <Zoom>
-                        <img src="/assets/images/SURATMAKLUMANCUTIDEEPAVALI2025.jpeg" alt="" />
-                    </Zoom>
-                </div>
-                 <div className="col-span-4 justify-items-center col-start-2 md:col-span-2 md:col-start-3" onClick={() => setIsOpen(true)}>
-                    <Zoom>
-                        <img src="/assets/images/MAJLISSOLATHAJATDANBACAANYASIN.jpeg" alt="" />
-                    </Zoom>
-                </div>
+  const images = [
+    '/assets/images/SuratMaklumanTaklimatPakejTingkatan4.jpeg',
+    '/assets/images/SURATMAKLUMANPERUBAHANJADUALPINTASTINGKATN42025.jpeg',
+    '/assets/images/SURATMAKLUMANCUTIDEEPAVALI2025.jpeg',
+    '/assets/images/MAJLISSOLATHAJATDANBACAANYASIN.jpeg',
+  ];
+
+  return (
+    <div className="bg-white">
+      <div className="pt-20 pb-20 px-5">
+        <h1 className="text-5xl text-center font-extrabold mb-10 tracking-widest text-[#90d6fa]">
+          SPM 2025
+        </h1>
+
+        {/* Responsive grid for images */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 max-w-6xl mx-auto">
+          {images.map((src, index) => (
+            <div key={index} className="w-full flex justify-center">
+              <Zoom>
+                <img
+                  src={src}
+                  alt={`Kejayaan ${index + 1}`}
+                  className="w-full h-auto max-h-[400px] object-cover rounded-xl shadow-md transition-transform duration-300 hover:scale-[1.02]"
+                />
+              </Zoom>
             </div>
+          ))}
         </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Makluman;
